04105c6f03759409247411bf94586b2a3633bafa
yasu-swift <sp4c72q9@icloud.com>
2021-09-12 14:26:30 +0900

    Ruby追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,7 +8,7 @@ class LanguageController extends Controller
 {
     public function index ()
     {
-        $language = ['HTML', 'CSS', 'JavaScript', 'PHP'];
+        $language = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby'];
         return view('language.index', ['language' => $language]);
     }
 }
